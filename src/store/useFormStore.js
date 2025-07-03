1d4ecb2425bfa83abee0136bf1b9b49944152f9b
nevin03 <nevinm2021@gmail.com>
2025-07-03 11:47:08 +0530

    chore(store): add Zustand form store for state management

---
+++ src/store/useFormStore.js
@@ -0,0 +1,33 @@
+// src/store/useFormStore.js
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+const useFormStore = create(
+  persist(
+    (set) => ({
+      formData: {},
+      isEditMode: false,
+
+      setFormData: (data) =>
+        set((state) => ({
+          formData: { ...state.formData, ...data },
+        })),
+
+      loadFormData: (data) => set({ formData: data }),
+
+      resetForm: () =>
+        set({
+          formData: {},
+          isEditMode: false,
+        }),
+
+      setEditMode: (isEdit) => set({ isEditMode: isEdit }),
+    }),
+    {
+      name: "user-form-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
+
+export default useFormStore;
